035a90e2c38b4811d453627e8f9e1bbb1c8120ed
Uygar Uyanıksoy <uygaruyaniksoy@gmail.com>
2018-10-01 22:19:22 +0300

    first build

--- Assets/Scripts/Axis.cs
+++ Assets/Scripts/Axis.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.Experimental.Audio.Google;
 using UnityEngine.Serialization;
 
 public class Axis : MonoBehaviour, IPointerClickHandler, SwipeHandler.Handler {
